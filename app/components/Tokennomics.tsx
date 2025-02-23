96ed6ab0e896ebc5de30d2c119f9dca6a675270d
Diamondhesit <diamondheisttm@gmail.com>
2025-02-23 22:26:54 +0000

    update

--- app/components/Tokennomics.tsx
+++ app/components/Tokennomics.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import { db } from "@/lib/firebase"; // Firestore instance
+import { doc, onSnapshot } from "firebase/firestore";
 
 interface TokenDistribution {
   name: string;
@@ -7,14 +10,23 @@ interface TokenDistribution {
 }
 
 const Tokenomics: React.FC = () => {
-  const distributions: TokenDistribution[] = [
-    { name: "Token Mined Globally", percentage: 10.2, color: "bg-green-500" }
-  ];
+  const [distributions, setDistributions] = useState<TokenDistribution[]>([]);
+
+  useEffect(() => {
+    const docRef = doc(db, "tokenomics", "distribution");
+
+    // Real-time listener
+    const unsubscribe = onSnapshot(docRef, (docSnap) => {
+      if (docSnap.exists()) {
+        setDistributions(docSnap.data().distributions);
+      }
+    });
+
+    return () => unsubscribe(); // Cleanup on unmount
+  }, []);
 
   return (
     <div className="bg-gradient-to-b from-gray-800 via-gray-800 to-gray-1000 rounded-lg p-6 backdrop-blur-md shadow-md w-full max-w-full text-white">
-      
-
       <h3 className="mt-4 text-xl text-center font-semibold">Token Distribution</h3>
       {distributions.map((item, index) => (
         <div key={index} className="mt-2 p-4 border-2 border-b-gray-700 border-t-0 border-r-0 border-l-0 bg-gradient-to-b from-gray-700 to-gray-900 rounded-lg">
